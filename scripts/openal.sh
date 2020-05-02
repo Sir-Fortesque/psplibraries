416c3e37cec7347d13eda446f1cb5aba1b5f52d1
Wouter Wijsman <wwijsman@live.nl>
2020-05-02 11:57:30 +0200

    Update version of OpenAL, doesn't compile yet

--- scripts/openal.sh
+++ scripts/openal.sh
@@ -3,15 +3,15 @@
 
 test_deps_install cmake-toolchain-script pthreads-emb
 
-OPENAL_VERSION=1.14
+OPENAL_VERSION=1.20.1
 
 download_and_extract http://kcat.strangesoft.net/openal-releases/openal-soft-$OPENAL_VERSION.tar.bz2 openal-soft-$OPENAL_VERSION
 
 ## replace memory.h with string.h
-sed -i -e "s/memory\.h/string.h/" Alc/*.c Alc/backends/*.c || { exit 1; }
+sed -i -e "s/memory\.h/string.h/" alc/*.cpp alc/backends/*.cpp || { exit 1; }
 
 ## Run cmake
-psp-cmake -D CMAKE_INSTALL_PREFIX=$(psp-config --psp-prefix) -D UTILS=OFF -D EXAMPLES=OFF . || { exit 1; }
+psp-cmake -DLIBTYPE=STATIC -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF . || { exit 1; }
 
 ## Compile and Install
 run_make -j `num_cpus`
